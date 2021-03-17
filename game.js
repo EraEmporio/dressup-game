6b9862e14b8c3e2037a21d17307b2363313b48b6
Bárbara Boechat <barbs.boechat@gmail.com>
2021-03-17 16:36:20 -0300

    função de seleção ok
    acredito que falta apenas mobile

---
+++ game.js
@@ -0,0 +1,103 @@
+let i = 0, char_escolhido = "", galeria = ""
+let images = ['tela01_homem-01.png', 'tela01_homem-02.png',
+    'tela01_mulher-01.png', 'tela01_mulher-02.png']
+
+$('.arrow').click(function () {
+
+    if ($(this).hasClass('arrow-left')) {
+        (i === 0) ? i = images.length - 1 : i--
+
+    } else {
+        (i === images.length - 1) ? i = 0 : i++
+    }
+    $(".main-character").css("background-image", "url(" + "tela1/galeria/" + images[i] + ")");
+
+});
+let j = {
+    'camisa': 0,
+    'calca': 0,
+    'calcado': 0,
+    'acessorio': 0,
+    'chapeu': 0
+}
+let k = 0
+$('.seta').click(function () {
+    let class_list = $(this).attr('class').split(/\s+/),
+        box = class_list[2].split('seta-').join('')
+
+    let objetos = $('.' + galeria + ' .' + box)
+
+    if ($(this).hasClass('seta-esquerda')) {
+        console.log('seta esquerda')
+        $(objetos[j[box]]).css('z-index', 0)
+        j[box] -= 1
+        if (j[box] < 0) j[box] = objetos.length - 1
+        $(objetos[j[box]]).css('z-index', 1)
+        console.log(j[box])
+    } else {
+        console.log('seta direita')
+        $(objetos[j[box]]).css('z-index', 0)
+        j[box] += 1
+        if (j[box] > objetos.length - 1) j[box] = 0
+        $(objetos[j[box]]).css('z-index', 1)
+        console.log(j[box])
+    }
+});
+
+$('#escolhe-char').click(function () {
+    let img_tela2 = ['tela02_homem-01.png', 'tela02_homem-02.png',
+        'tela02_mulher-01.png', 'tela02_mulher-02.png']
+
+    char_escolhido = img_tela2[i];
+    (char_escolhido.includes('mulher')) ? galeria = 'galeria-mulher' : galeria = 'galeria-homem'
+
+    $("#char").attr("src", "tela2/principal/" + char_escolhido);
+    $('#tela01').hide();
+    $('#tela02').show();
+});
+
+$('.select').click(function () {
+    let class_list = $(this).closest("div")
+        .find('img').attr('class').split(/\s+/)
+
+    let box = class_list[2].split('seta-').join(''),
+        objetos = $('.' + galeria + ' .' + box)
+
+    let main_char = $('#' + box + '-on'),
+        calcado_on = $('#calcado-on'),
+        camisa =  $('#camisa-on'),
+        source_img = $(objetos[j[box]]).attr('src')
+
+    let sapato_astro = source_img.includes('sapato-astronauta'),
+        sapato_bomb = source_img.includes('sapato-bombeiro'),
+        calca_jard_on = $('#calca-on').attr('src').includes('calca-jardineiro'),
+        calca_jard = source_img.includes('calca-jardineiro')
+
+    if (box === 'calcado' && sapato_bomb || sapato_astro) {
+        let z_index = 0
+        if (!calca_jard_on) z_index = 1
+        main_char.css('z-index', z_index)
+    } else if (box === 'calcado') {
+        main_char.css('z-index', 0)
+    } else if (box === 'calca' && calca_jard) {
+        calcado_on.css('z-index', 0)
+    } else {
+        calcado_on.css('z-index', 1)
+    }
+
+    if (calca_jard) {
+       camisa.attr('src', ' ')
+    }
+
+    if (!(box === 'camisa' && calca_jard_on)) {
+        main_char.attr(
+            'src',
+            source_img.replace('galerias', 'principal')
+        )
+    }
+});
+
+$(document).ready(function () {
+    $('#exampleModal').modal('show')
+    $('#tela02').hide();
+});
